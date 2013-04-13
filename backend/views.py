b8315739f8a7fb35e651c35552611975d6b6b5ee
Owen Campbell-Moore <owencmoore@gmail.com>
2013-04-13 02:22:40 +0100

    Viewing individual qaitems

--- backend/views.py
+++ backend/views.py
@@ -5,11 +5,16 @@ from django.utils import simplejson
 
 from backend.models import QaItem
 
+def serialiseItem(qaitem):
+	return {"question": qaitem.question, "answer": qaitem.answer}
+
 def qaitem_index(request):
 	qaitems = QaItem.objects.all()
-	qaitems_serialised = map(lambda qaitem: {"question": qaitem.question, "answer": qaitem.answer} , qaitems)
+	qaitems_serialised = map(serialiseItem, qaitems)
 	output_json = simplejson.dumps(qaitems_serialised)
 	return HttpResponse(output_json, mimetype='application/json')
 
 def qaitem(request, qaitem_id):
-	return HttpResponse("Hello, world. You're viewing qaitem %s." % qaitem_id) 
+	qaitem = serialiseItem(QaItem.objects.filter(id=qaitem_id)[0])
+	output_json = simplejson.dumps(qaitem)
+	return HttpResponse(output_json, mimetype='application/json') 
