d2aa3428eecd095e5050bce272f39ff95f0c1f06
Owen Campbell-Moore <owencmoore@gmail.com>
2013-04-13 09:14:16 +0100

    Fixed rest dependency

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-from rest_framework.urlpatterns import format_suffix_patterns
+# from rest_framework.urlpatterns import format_suffix_patterns
 # from backend.views import QaItemList, QaItemDetail
 
 from backend import views
